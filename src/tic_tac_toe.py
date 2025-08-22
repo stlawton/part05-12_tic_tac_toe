2a067a0a13f41c34da66b094566f797754904673
stlawton <lawtons347@gmail.com>
2025-08-22 16:48:33 -0400

    Create function play_turn

--- src/tic_tac_toe.py
+++ src/tic_tac_toe.py
@@ -1,1 +1,7 @@
-# Write your solution here
+def play_turn(game_board: list, x: int, y: int, piece: str):
+  if x >= 3 or y >= 3:
+    return False
+  if game_board[y][x] != "":
+    return False
+  game_board[y][x] = piece
+  return True
