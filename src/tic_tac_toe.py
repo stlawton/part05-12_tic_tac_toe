bcaf4ed541d92eea626669904f47a6211b4a818e
stlawton <lawtons347@gmail.com>
2025-08-23 10:50:10 -0400

    Passed tests

--- src/tic_tac_toe.py
+++ src/tic_tac_toe.py
@@ -1,6 +1,8 @@
 def play_turn(game_board: list, x: int, y: int, piece: str):
   if x >= 3 or y >= 3:
     return False
+  if x < 0 or y < 0:
+    return False
   if game_board[y][x] != "":
     return False
   game_board[y][x] = piece
